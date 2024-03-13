7eff1bc1c44fcaa48ff21d8c7c3fbde8f0b3831c
ps833256 <ps833256@hmail.com>
2024-03-14 00:43:55 +0530

    UPDATE

--- Homepage/home.js
+++ Homepage/home.js
@@ -15,7 +15,6 @@ document.getElementById("lanselectdiv").addEventListener("mouseout", function(){
 });
 
 
-
 document.getElementById("signindiv").addEventListener("mouseover",function(){
 
     document.getElementById("signinhoverdiv").style.display= "block";
@@ -40,3 +39,40 @@ document.getElementById("crossicon").addEventListener("click",function(event){
     
     document.getElementById("allhoverdiv").style.display= "none";
 })
+
+
+let data = [
+    { image: "https://images-eu.ssl-images-amazon.com/images/G/31/OHL/24/BAU/feb/PC_hero_1_2x_1._CB582889946_.jpg"},
+    { image: "https://images-eu.ssl-images-amazon.com/images/G/31/img2020/img21/apparelGW/augatf23/hsbcunrec/MA_2x._CB580708889_.jpg" },
+    { image: "https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/GW/Uber/Nov/D103625178_DesktopTallHero_3000x1200._CB574597993_.jpg" },
+    { image: "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Consumables/X-GL/Feb5/PC_Hero_1_3000._CB582457311_.jpg" },
+    { image: "https://images-eu.ssl-images-amazon.com/images/G/31/saba/branddays/5300---Kitchen---Citi-bank-strip._CB580044345_.png" },
+    { image: "https://images-eu.ssl-images-amazon.com/images/G/31/saba/branddays/5300---Kitchen---Citi-bank-strip._CB580044345_.png" },
+    { image: "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Softlines_JWL_SH_GW_Assets/jan2024/Bau/Unrec/BOB/SHOES/Frame_386_3000pc._CB581979262_.jpg" },
+    { image: "https://images-eu.ssl-images-amazon.com/images/G/31/img22/WLA/2024/BAU/Hero/D91435399_WLA-BAU-Unrec-Hero_DesktopTallHero_3000x1200-bau._CB582195367_.jpg" }
+
+  ];
+
+  let currentIndex = 0;
+  function changeImage() {
+    document.getElementById("cont").innerHTML = ''; 
+
+    let img = document.createElement("img");
+    img.src = data[currentIndex].image;
+    img.setAttribute("width", "100%")
+    img.setAttribute("height", "500px")
+    cont.append(img);
+
+    currentIndex = (currentIndex + 1) % data.length; 
+  }
+
+
+  document.getElementById("righticon").addEventListener("click", changeImage);
+
+  changeImage();
+
+  document.getElementById("lefticon").addEventListener("click", changeImage);
+
+  changeImage();
+
+
